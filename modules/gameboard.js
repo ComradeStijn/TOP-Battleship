50b9937c90c9fb365a01db9ea5b474d0f5b5a599
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-21 18:08:47 +0200

    feat: Placeship functionality and testing

--- modules/gameboard.js
+++ modules/gameboard.js
@@ -1,16 +1,16 @@
-import { Ship } from "./ship.js"
+import { Ship } from "./ship.js";
+const ROWS = 10;
+const COLUMNS = 10;
 
 export class Gameboard {
   constructor() {
-    const rows = 10;
-    const columns = 10;
-    const emptyCell = {
+    const emptyCell = () => ({
       ship: null,
-      filled: false
-    }
-    this.gameBoard = Array(rows)
+      filled: false,
+    });
+    this.gameBoard = Array(ROWS)
       .fill()
-      .map(() => Array(columns).fill(emptyCell));
+      .map(() => Array(COLUMNS).fill().map(emptyCell));
   }
 
   changeValueAtCoordinate(x, y, value) {
@@ -20,5 +20,58 @@ export class Gameboard {
   getStatusFromCoordinate(x, y) {
     return this.gameBoard[x][y];
   }
-}
 
+  placeShip(x, y, length, axis) {
+    if (
+      this.checkLengthInvalid(x, y, length, axis) ||
+      this.checkShipExists(x, y, length, axis)
+    ) {
+      return false;
+    }
+
+    const ship = new Ship(length);
+    if (axis === "x") {
+      for (let i = 0; i < length; i++) {
+        this.gameBoard[x][y + i].ship = ship;
+        this.gameBoard[x][y + i].filled = true;
+      }
+      return true;
+    } else if (axis === "y") {
+      for (let i = 0; i < length; i++) {
+        this.gameBoard[x + i][y].ship = ship;
+        this.gameBoard[x + i][y].filled = true;
+      }
+      return true;
+    } else return false;
+  }
+
+  checkShipExists(x, y, length, axis) {
+    if (axis === "x") {
+      for (let i = 0; i < length; i++) {
+        if (this.gameBoard[x][y + i].filled === true) {
+          return true;
+        }
+      }
+      return false;
+    } else if (axis === "y") {
+      for (let i = 0; i < length; i++) {
+        if (this.gameBoard[x][y + i].filled === true) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      throw new Error("Error with checkShipExists function in gameboard.js");
+    }
+  }
+
+  checkLengthInvalid(x, y, length, axis) {
+    if (axis === "x") {
+      return y + length > COLUMNS;
+    } else if (axis === "y") {
+      return x + length > ROWS;
+    } else {
+      throw new Error("Error with checkLengthInvalid function in gameboard.js");
+    }
+  }
+}
--- modules/gameboard.test.js
+++ modules/gameboard.test.js
@@ -1,26 +1,98 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect, beforeEach } from "@jest/globals";
 import { Gameboard } from "./gameboard";
+import { Ship } from "./ship";
 
 describe("Gameboard initializing", () => {
+  let testBoard;
+  beforeEach(() => {
+    testBoard = new Gameboard();
+  });
+
   test("Gameboard exists", () => {
-    const testBoard = new Gameboard();
     expect(testBoard.gameBoard).not.toBeUndefined();
   });
 
   test("Can change and retrieve value of x,y coordinate", () => {
-    const testBoard = new Gameboard();
     testBoard.changeValueAtCoordinate(2, 3, "test");
     expect(testBoard.getStatusFromCoordinate(2, 3)).toBe("test");
   });
 
   test("Can retrieve value of x, y coordinate", () => {
-    const testBoard = new Gameboard();
     const expectedResult = {
       ship: null,
       filled: false,
-    }
+    };
     expect(testBoard.getStatusFromCoordinate(0, 0)).toEqual(expectedResult);
-  })
+  });
 });
 
+describe("Placing ships horizontally", () => {
+  let testBoard;
+  beforeEach(() => {
+    testBoard = new Gameboard();
+  });
 
+  test("Ship is placed on 0,0 coordinate", () => {
+    expect(testBoard.placeShip(0, 0, 3, "x")).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(0, 0).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(0, 0).ship).toBeInstanceOf(Ship);
+  });
+
+  test("Placed ship has correctly initialised its length", () => {
+    testBoard.placeShip(0, 0, 3, "x");
+    expect(testBoard.getStatusFromCoordinate(0, 0).ship.getLength()).toBe(3);
+  });
+
+  test("Placing ship horizontally works", () => {
+    testBoard.placeShip(0, 0, 3, "x");
+
+    expect(testBoard.getStatusFromCoordinate(0, 0).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(0, 1).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(0, 2).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(0, 3).filled).toBe(false);
+  });
+
+  test("If ship already present, do not place ship", () => {
+    testBoard.changeValueAtCoordinate(0, 1, { filled: true });
+    testBoard.placeShip(0, 0, 3, "x");
+    expect(testBoard.getStatusFromCoordinate(0, 0).filled).toBe(false);
+    expect(testBoard.placeShip(0, 0, 3, "x")).toBe(false);
+  });
+
+  test("If length exceeds boundary, do not place ship", () => {
+    expect(testBoard.placeShip(0, 9, 1, "x")).toBe(true);
+    expect(testBoard.placeShip(1, 9, 2, "x")).toBe(false);
+    expect(testBoard.placeShip(2, 8, 2, "x")).toBe(true);
+    expect(testBoard.placeShip(3, 8, 3, "x")).toBe(false);
+  });
+});
+
+describe("Placing ships vertically", () => {
+  let testBoard;
+  beforeEach(() => {
+    testBoard = new Gameboard();
+  });
+
+  test("Placing ship vertically works", () => {
+    testBoard.placeShip(0, 0, 3, "y");
+
+    expect(testBoard.getStatusFromCoordinate(0, 0).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(1, 0).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(2, 0).filled).toBe(true);
+    expect(testBoard.getStatusFromCoordinate(3, 0).filled).toBe(false);
+  });
+
+  test("If ship already present, do not place ship", () => {
+    testBoard.changeValueAtCoordinate(0, 1, { filled: true });
+    testBoard.placeShip(0, 0, 3, "y");
+    expect(testBoard.getStatusFromCoordinate(0, 0).filled).toBe(false);
+    expect(testBoard.placeShip(0, 0, 3, "y")).toBe(false);
+  });
+
+  test("If length exceeds boundary, do not place ship", () => {
+    expect(testBoard.placeShip(9, 0, 1, "y")).toBe(true);
+    expect(testBoard.placeShip(9, 1, 2, "y")).toBe(false);
+    expect(testBoard.placeShip(8, 2, 2, "y")).toBe(true);
+    expect(testBoard.placeShip(8, 3, 3, "y")).toBe(false);
+  });
+});
