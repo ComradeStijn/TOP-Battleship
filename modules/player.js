705aca326605ce2a2c31aae578bd66c179b9aecc
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-10-22 21:39:05 +0200

    feat: Player class places 5 ships

--- modules/player.js
+++ modules/player.js
@@ -1,8 +1,29 @@
-import { Gameboard } from "./gameboard";
+import { Gameboard } from "./gameboard.js";
 
 export class Player {
   constructor(playable) {
     this.playable = playable === "pc";
     this.gameBoard = new Gameboard();
+    this.placeAllShips();
+  }
+
+  placeAllShips() {
+    let shipCount = 0;
+    const shipSizes = [2, 3, 3, 4, 5];
+    while (shipCount < 5) {
+      const randomCoordinateX = Math.floor(Math.random() * 10);
+      const randomCoordinateY = Math.floor(Math.random() * 10);
+      const randomAxis = Math.round(Math.random()) === 0 ? "x" : "y";
+      if (
+        this.gameBoard.placeShip(
+          randomCoordinateX,
+          randomCoordinateY,
+          shipSizes[shipCount],
+          randomAxis
+        )
+      ) {
+        shipCount++;
+      }
+    }
   }
 }
--- modules/player.test.js
+++ modules/player.test.js
@@ -1,6 +1,6 @@
 import { Player } from "./player";
 import { Gameboard } from "./gameboard";
-import { describe, test, expect, beforeAll, beforeEach } from "@jest/globals";
+import { describe, test, expect, beforeEach } from "@jest/globals";
 
 describe("Player initializing", () => {
   let testPlayer
@@ -20,5 +20,16 @@ describe("Player initializing", () => {
 
   test("Each player has a gameboard",() => {
     expect(testPlayer.gameBoard).toBeInstanceOf(Gameboard);
+  });
+
+  test("Each player's gameboard has 5 ships", () => {
+    expect(testPlayer.gameBoard.ships.length).toBe(5);
+  });
+
+  test("Each player's gameboard has 5 ships with length 2, 3, 3, 4, 5", () => {
+    const comparison = [2, 3, 3, 4, 5];
+    const shipsArray = testPlayer.gameBoard.ships;
+    const shipLengths = shipsArray.map((ship) => ship.getLength()).sort();
+    expect(shipLengths).toEqual(comparison);
   })
 });
